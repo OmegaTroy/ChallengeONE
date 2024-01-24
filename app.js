dad29376060b12fd08d647ab406efdde1d4230ef
OmegaTroy <juli.mjoi44@gmail.com>
2024-01-24 06:25:06 -0300

    sacar los acentos

--- app.js
+++ app.js
@@ -4,42 +4,24 @@ const $desencrip = document.getElementById('desencrip')
 const $textEncryp = document.querySelector('.text-encrip')
 const $btnCopy = document.querySelector('.btn__copi')
 
-const letterMap = new Map([
-  ['a','ai'],
-  ['e','enter'],
-  ['i','imes'],
-  ['o','ober'],
-  ['u','ufat'],
-])
-
-//* funsion de encriptado
-// const encrip=(letter)=>{
-//   let encrypText = ''
-//   for(let i = 0; i < letter.length; i++){
-//     if(letterMap.has(letter.charAt(i))){
-//       encrypText += letterMap.get(letter.charAt(i))
-//     }else{
-//       encrypText += letter.charAt(i)
-//     }
-//   }
-//   return encrypText
-// }
-
-
 
 $encrip.addEventListener('click',()=>{
   let letter = $textarea.value.toLowerCase()
   if(letter === ''){
     return $textarea.focus()
   }
-  let encrypText = ''
-  for(let i = 0; i < letter.length; i++){
-    if(letterMap.has(letter.charAt(i))){
-      encrypText += letterMap.get(letter.charAt(i))
-    }else{
-      encrypText += letter.charAt(i)
-    }
-  }
+
+  const sinAcentos = letter.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  const reglas = {
+    'e': 'enter',
+    'i': 'imes',
+    'a': 'ai',
+    'o': 'ober',
+    'u': 'ufat'
+  };
+
+  // Aplicar las reglas de encriptación al texto
+  const encrypText = sinAcentos.replace(/[aeiou]/g, letra => reglas[letra] || letra);
   $textarea.value = ''
   $textEncryp.textContent = encrypText
 })
@@ -51,15 +33,16 @@ $desencrip.addEventListener('click',()=>{
     return $textarea.focus()
   }
 
+  const reglasInversas = {
+    'enter': 'e',
+    'imes': 'i',
+    'ai': 'a',
+    'ober': 'o',
+    'ufat': 'u'
+  };
+
   let descryp = ''
-  for(let i = 0; i < letter.length; i++){
-    if(letterMap.has(letter.charAt(i))){
-      descryp += letter.charAt(i)
-      i += ((letterMap.get(letter.charAt(i))).length)-1
-    }else{
-      descryp += letter.charAt(i)
-    }
-  }
+  descryp += letter.replace(/(enter|imes|ai|ober|ufat)/g, match => reglasInversas[match] || match);
   $textarea.value = ''
   $textEncryp.textContent = descryp
 })
